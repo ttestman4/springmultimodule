fd941c7f8ee970af1ff357de6d59277148570163
ttestman4 <ttestman4@gmail.com>
2024-02-14 07:28:56 +0530

    Add REST function in kafka processor

--- spring-cloud-stream-app11/src/main/java/com/example/name/Processor.java
+++ spring-cloud-stream-app11/src/main/java/com/example/name/Processor.java
@@ -4,14 +4,21 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.example.common.Employee;
 
 @Configuration
 public class Processor {
+    private final WebClient client;
+
+	public Processor(WebClient.Builder builder) {
+        this.client = builder.baseUrl("http://localhost:8096").build();
+    }
 
 	@Bean
 	public Function<Message<Employee>, Message<Employee>> processName() {
@@ -21,6 +28,26 @@ public class Processor {
 					// Propagate the partion key
 					.setHeader(KafkaHeaders.KEY, msg.getHeaders().get(KafkaHeaders.RECEIVED_KEY))
 					.build();
+		};
+	}
+
+	@Bean
+	public Function<Message<Employee>, Message<Employee>> processNameViaRest() {
+		return (msg) -> {
+			Employee empIn = msg.getPayload();
+			Employee empOut = this.client.post()
+                .uri("/employees/name")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(empIn)
+                .retrieve()
+                .bodyToMono(Employee.class)
+                // .delayElement(Duration.ofSeconds(100))
+                .block();
+			
+			return MessageBuilder.withPayload(empOut)
+				// Propagate the partion key
+				.setHeader(KafkaHeaders.KEY, msg.getHeaders().get(KafkaHeaders.RECEIVED_KEY))
+				.build();
 		};
 	}
 }
