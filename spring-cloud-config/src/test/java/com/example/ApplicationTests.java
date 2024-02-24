7a30c35b6050c6abe16c58b820a1d4e18b78478f
ttestman4 <ttestman4@gmail.com>
2024-02-24 10:27:04 +0530

    small fixes for config testing

--- spring-cloud-config/src/test/java/com/example/ApplicationTests.java
+++ spring-cloud-config/src/test/java/com/example/ApplicationTests.java
@@ -11,6 +11,21 @@ class ApplicationTests {
 	}
 
 	// Manual Test
-	// curl -X POST http://localhost:8080/
-	
+	// curl localhost:8888/foo/dev | jq
+
+// 	The HTTP service has resources in the following form:
+
+// /{application}/{profile}[/{label}]
+// /{application}-{profile}.yml
+// /{label}/{application}-{profile}.yml
+// /{application}-{profile}.properties
+// /{label}/{application}-{profile}.properties
+// For example:
+
+// curl localhost:8888/foo/development
+// curl localhost:8888/foo/development/master
+// curl localhost:8888/foo/development,db/master
+// curl localhost:8888/foo-development.yml
+// curl localhost:8888/foo-db.properties
+// curl localhost:8888/master/foo-db.properties
 }
